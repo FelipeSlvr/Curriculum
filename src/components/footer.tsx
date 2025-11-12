a38f41a8d92e18e27e4fa7c15ebd5e54a8733719
Felipe Silveira <felipe-forza@outlook.com>
2025-11-12 11:43:59 -0300

    Localize footer text and update Portuguese translations
    Footer component now uses i18next for localized text. Updated Portuguese locale file with revised experience and education sections, including new descriptions and university names.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,14 @@
+import { useTranslation } from 'react-i18next';
+
 const Footer = () => {
+
+    const { t } = useTranslation();
 
     return (
         <footer className="bg-zinc-950 border-t border-zinc-800 p-3 mx-auto bottom-0" >
             <div className="container mx-auto flex justify-between items-center">
-                <p className="p-4 text-lg text-white">Copyright 2024 - 2025 Felipe Silveira. <span className="text-white/80">All rights reserved</span></p>
-                <p className="p-4 text-lg text-white"> This Website was developed using <span className="underline underline-offset-2 font-bold">React and TailwindCSS + Shadcn</span>.</p>
+                <p className="p-4 text-lg text-white">Copyright 2024 - 2025 Felipe Silveira. <span className="text-white/80">{t('footer.rights')}</span></p>
+                <p className="p-4 text-lg text-white"> {t('footer.builtWith')} <span className="underline underline-offset-2 font-bold">React, TailwindCSS + Shadcn</span>.</p>
             </div>
         </footer>
     );
